0baac494a2eafc01537f05dfc77de2c72d17a772
Emeson12345678910 <124007178+Emeson12345678910@users.noreply.github.com>
2023-03-09 20:46:55 -0600

    Add files via upload

---
+++ Actividad Asincrona/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Datos
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingresa un numero entero porfavor: ");
+        int E = Convert.ToInt32(Console.ReadLine());
+
+        for (int indice = 0; indice < E; indice++)
+        {
+            for (int cr7 = 0; cr7 <= indice; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
